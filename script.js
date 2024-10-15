f6fe94e9a5065ac0439cf15762ca0f237472e5e3
kelvinempire <kelvinewurum@gmail.com>
2024-10-15 15:59:11 +0100

    form-update

---
+++ script.js
@@ -0,0 +1,36 @@
+const formEl = document.querySelector("form");
+console.log(formEl);
+function validateForm(e) {
+  e.preventDefault();
+  const firstNameEl = document.getElementById("firstName").value;
+  const secondNameEl = document.getElementById("secondName").value;
+  const emailEl = document.getElementById("email").value;
+  const passwordEl = document.getElementById("password").value;
+  const checkboxEl = document.getElementById("checkbox").checked;
+  if (firstNameEl === "") {
+    alert("first name is required");
+  }
+
+  if (secondNameEl === "") {
+    alert("second name is required");
+  }
+  if (emailEl === "") {
+    alert("email is required");
+  }
+  if (passwordEl === "") {
+    alert("password is required");
+  }
+  if (checkboxEl === "") {
+    alert("do you accept the terms and condition");
+  }
+  if (
+    firstNameEl !== "" &&
+    secondNameEl !== "" &&
+    emailEl !== "" &&
+    passwordEl !== "" &&
+    checkboxEl === true
+  ) {
+    alert("form submitted successfully");
+  }
+}
+formEl.addEventListener("submit", validateForm);
